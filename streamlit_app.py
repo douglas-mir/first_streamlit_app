6d1f16a903230ad781ef2c6227d8d01b2589a7eb
Douglas Miranda <55262386+douglas-mir@users.noreply.github.com>
2022-11-16 14:20:30 -0300

    Update streamlit_app.py
    second entry box

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The Fruit load list contains:")
 streamlit.text(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
